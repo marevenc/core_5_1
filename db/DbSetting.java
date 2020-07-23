431b13a045661648883a49c66620bcedb6f2a543
Mariya Kosheleva <marevenc@gmail.com>
2020-07-23 22:55:24 +0300

    Initial commit

---
+++ db/DbSetting.java
@@ -0,0 +1,11 @@
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public Db(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
